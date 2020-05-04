830ec47466ffa268d60f1dfd831cfa7842918682
Eduardo H. M. Cruz <eduardohmdacruz@gmail.com>
2020-05-04 17:24:44 -0300

    calculating estimated r0 in r

--- gen-graphics.sh
+++ gen-graphics.sh
@@ -5,6 +5,8 @@
 # 	exit
 # fi
 
+gsize="8"
+
 #sceneries="pvai-do-nothing pvai-2-waves"
 sceneries="pvai-do-nothing"
 #sceneries="palmas-do-nothing palmas-2-waves"
@@ -12,7 +14,20 @@ sceneries="pvai-do-nothing"
 
 for test in $sceneries
 do
-	./log/calc-mean-stddev.r log/results/results-$test log/results/results-$test
+	rm -rf log/results/processed-$test
+	mkdir log/results/processed-$test
+
+	for f in `ls log/results/results-$test`
+	do
+		fin="log/results/results-$test/$f"
+		fout="log/results/processed-$test/$f"
+		echo "$fin -> $fout"
+		./log/calculate-stuff.r $fin $fout $gsize
+	done
+
+#	exit
+
+	./log/calc-mean-stddev.r log/results/processed-$test log/results/results-$test
 	
 	./log/plot-with-interval.r log/results/results-$test-mean.csv log/results/results-$test-se.csv log/results/results-$test
 done
---
+++ log/calculate-stuff.r
@@ -0,0 +1,52 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args) != 3)
+	stop("Usage: plot-cycles.R <input.csv> <output.csv> <group_size>\n")
+
+csv_input = args[1]
+csv_output = args[2]
+group_size = as.integer(args[3])
+
+data = read.csv(csv_input, header = TRUE)
+
+print(data)
+
+col_reproductive <- numeric(nrow(data))
+col_group_infected <- numeric(nrow(data))
+
+print(nrow(data))
+#print(col_group_infected)
+print(group_size)
+
+for (i in 1:length(col_reproductive)) {
+	ini = i - group_size + 1
+	if (ini < 1)
+		ini <- 1
+	iniref = ini - 1
+
+	col_group_infected[i] <- 0
+
+	for (j in ini:i) {
+		col_group_infected[i] <- col_group_infected[i] + data[j, "state_ST_INFECTED"]
+
+		if (iniref < 1)
+			col_reproductive[i] <- 0
+		else if (col_group_infected[iniref] == 0)
+			col_reproductive[i] <- 0
+		else
+			col_reproductive[i] <- col_group_infected[i] / col_group_infected[iniref]
+	}
+}
+
+#print(col_reproductive)
+
+data$group_infected <- col_group_infected
+data$reproductive <- col_reproductive
+
+#data_input$test <- data_input$state_ST_INFECTED
+
+print(data)
+
+write.csv(data, csv_output)
